ce3b0281addb2b50413f451b1bfba066abc7e1b8
Artem Abramov <notdepot@gmail.com>
2024-09-29 10:06:00 +0000

    Added locked decorator for custom router

--- app/routers/custom_execute_router.py
+++ app/routers/custom_execute_router.py
@@ -7,6 +7,7 @@ from app.cache import get_cache
 from app.auth import auth
 from app.models.user_model import User, UserRole
 from app.constants import HOOK_ON_CUSTOM_EXECUTE
+from app.decorators.locked_decorator import locked
 
 router = APIRouter()
 
@@ -14,6 +15,7 @@ router = APIRouter()
 @router.post("/custom", summary="Execute custom handler",
              response_class=JSONResponse, status_code=status.HTTP_200_OK,
              tags=["Services"])
+@locked
 async def custom_execute(
     schema: CustomExecuteRequest,
     session=Depends(get_session), cache=Depends(get_cache),
